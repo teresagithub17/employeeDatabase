12ffd6bd52a9ee69743c4001274b0a4839a8be9b
teresa <teresa.pilapil@gmail.com>
2023-05-09 23:39:24 +1000

    Updated employee table data.

--- db/schema.sql
+++ db/schema.sql
@@ -7,19 +7,19 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INTEGER NOT NULL
-    FOREIGN KEY (department)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    department_id INTEGER
+    -- FOREIGN KEY (department)
+    -- REFERENCES department(id)
+    -- ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INTEGER NOT NULL,
+    role_id INTEGER,
     manager_id INTEGER
-    FOREIGN KEY (role)
-    REFERENCES role(id)
-    ON DELETE SET NULL
+    -- FOREIGN KEY (role)
+    -- REFERENCES role(id)
+    -- ON DELETE SET NULL
 );
