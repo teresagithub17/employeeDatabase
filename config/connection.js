d201c28f539f08a8dcf06467d0bc88b37bb0277f
teresa <teresa.pilapil@gmail.com>
2023-05-09 21:21:14 +1000

    Initial commit.

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// Get the client
+const mysql = require('mysql2');
+// Create the connection
+const con = mysql.createConnection({
+    host:'127.0.0.1', 
+    user: 'root', 
+    password: 'B00tc@mp2023',
+    database: 'employee_db'
+});
+
+con.promise().query("SELECT 1")
+  .then( ([rows,fields]) => {
+    console.log(rows);
+  })
+  .catch(console.log)
+  .then( () => con.end());
+
+  module.exports = db;
